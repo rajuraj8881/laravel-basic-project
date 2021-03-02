9950bb187ee137ba0accb950a01b513bf862b38f
Raju Mondal <45078493+rajuraj8881@users.noreply.github.com>
2021-03-02 22:24:54 +0600

    Blade Templating

---
+++ app/Http/Controllers/FrontEnd/FrontController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+}
